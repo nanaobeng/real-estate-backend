d5e6b59ee37b74e46ac2d56af2ce93c1c5642d05
nanaobeng <nanaobengmarnu@gmail.com>
2021-08-20 18:16:02 +0000

    Server file

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+//routes
+
+
+
+app.listen(5000, () => {
+  console.log(`Server is starting on port 5000`);
+});
