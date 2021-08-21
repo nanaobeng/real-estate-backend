47537c636d67025f073f0c2b0ca4052cfb4e1d3b
nanaobeng <nanaobengmarnu@gmail.com>
2021-08-21 13:32:02 +0000

    multiple image files

--- controllers/listing.js
+++ controllers/listing.js
@@ -8,11 +8,15 @@ const authorize = require("../utils/authorize");
 const formidable = require('formidable');
 const _ = require('lodash');
 const fs = require('fs');
-
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3({
+    accessKeyId:process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
 
 
 exports.addListing =  async (req,res) => {
-
+    try {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
     form.multiples = true
@@ -30,13 +34,60 @@ exports.addListing =  async (req,res) => {
              
              
             } = fields;
-            for(var i = 0; i<files.image.length ; i++){
-                console.log(files.image[i].path)
-            }
+
+        //insert listing
+
+        const newLocation = await pool.query(
+                  "INSERT INTO listing (listing_title,thumbnail,description,property_type,rooms,has_parking,available_for_sale,available_for_rent,sale_price,location_id,status) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING *",
+                  [listing_title,'test thumb' ,description,property_type,
+                    rooms,has_parking,available_for_sale,available_for_rent,
+                    sale_price,location_id,'pending']
+                );
+            
+                res.json(newLocation.rows[0]);
+
+
+        //     for(var i = 0; i<files.image.length ; i++){
+                    
+        // let fileName = files.image.path
+
+            
+        // // Read content from the file
+        // const fileContent = fs.readFileSync(fileName);
+    
+        // // Setting up S3 upload parameters
+        // const params = {
+        //     Bucket: 'swifthomes',
+        //     Key: files.photo.name, // File name you want to save as in S3
+        //     Body: fileContent
+        // };
+
+        
+    
+        // // Uploading files to the bucket
+        // s3.upload(params, function(err, data) {
+        //     if (err) {
+        //         throw err;
+        //     }
+        //     console.log(`File uploaded successfully. ${data.Location}`);
+            
+        //     // uploading to postgress
+        //     const newImage = await pool.query(
+        //               "INSERT INTO images(listing_id,url) VALUES($1,$2) RETURNING *",
+        //               [listing_id,(data.location)]
+        //             );
+            
+        // });
+        //     }
         }
       
 
     })
+}
+catch (err) {
+       console.error(err.message);
+      }
+
 
     
     // try {
