7fe10561364912bd284688fdebbb5efdda97f216
nanaobeng <nanaobengmarnu@gmail.com>
2021-08-21 09:19:48 +0000

    delete  location  controller add valiidation route

--- routes/location.js
+++ routes/location.js
@@ -6,13 +6,13 @@ const pool = require("../db");
 const jwtGenerator = require("../utils/jwtGenerator");
 const authorize = require("../utils/authorize");
 
-const {addLocation,updateLocation,getLocation} = require('../controllers/location');
+const {addLocation,updateLocation,getLocation,deleteLocation} = require('../controllers/location');
 const { isAuth } = require("../controllers/auth");
 
 
 router.post("/admin/location/add", authorize,addLocation);
 router.put("/admin/location/update/:id", authorize,updateLocation);
 router.get("/location/:id",getLocation);
-router.delete("/admin/location/:id",authorize,getLocation);
+router.delete("/admin/location/:id",authorize,deleteLocation);
 
 module.exports = router;
