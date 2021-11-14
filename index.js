bbe56907a0b6ac3822c86debcccff77014d51e36
nanaobeng <nanaobengmarnu@gmail.com>
2021-11-14 12:52:28 +0000

    port change

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ app.use(dashboardRoutes)
 
 
 
-app.listen(5000, () => {
-  console.log(`Server is currently running on port 5000`);
-});
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
