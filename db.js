53a70634b581fffe9cc785bcca4af220508b6496
Nana Obeng-Marnu <30693400+nanaobeng@users.noreply.github.com>
2022-06-15 11:35:45 +0000

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  host: 'tyke.db.elephantsql.com' ,
-  user: 'rbrssgox' ,
-  password: 'SG9jx1l9opJl8bMl5IYo9wVWjQWhPE8z',
-  port: 5432,
-  database: 'rbrssgox'
+ 
 });
 
 module.exports = pool;
